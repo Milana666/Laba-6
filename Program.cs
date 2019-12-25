704daae3a08d1e6fdbaa18a0a713afefbe8e49bb
Milana666 <milana20002003@gmail.com>
2019-12-25 14:49:04 +0400

    Mess

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        const int N_max = 1000000;  // максимальная размерность массива
+        static double[] a = new double[N_max];  // объявление массива
+
+       
+    }
+}
